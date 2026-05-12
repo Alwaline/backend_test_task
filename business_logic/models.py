b24b0fb9e9ac1d88d877ade1dc801f3d6f1d73ea
alwaline <02ndrey@gmail.com>
2026-05-12 16:45:11 +0300

    fix get user model

--- business_logic/models.py
+++ business_logic/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-
-User = get_user_model()
+from users.models import User
 
 class Orders(models.Model):
     name = models.CharField(max_length=100)

da044be488bfe6a0f36541f94b622778137cf074
alwaline <02ndrey@gmail.com>
2026-05-12 14:08:47 +0300

    added user module

---
+++ backend_tt/users/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    first_name = models.CharField("Имя", max_length=255)
+    last_name = models.CharField("Фамилия", max_length=255)
+    patronymic = models.CharField("Отчество", max_length=255, blank=True)
+    email = models.EmailField("Email", max_length=255, unique=True)
+    is_active = models.BooleanField("Активен", default=True)
+    role = models.ForeignKey(
+        "roles.Role",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="users",
+    )
+    objects = UserManager()
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
